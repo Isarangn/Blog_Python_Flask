78ad5660295e06dd68407e4fa694276752d04592
sarang <sarang.newaskar@gmail.com>
2020-08-18 00:00:19 +0530

    Delate function is working in dashboard

--- main.py
+++ main.py
@@ -123,6 +123,13 @@ def logout():
     session.pop('user')
     return redirect('/dashboard')
 
+@app.route("/delete/<string:sno>", methods = ['GET', 'POST'])
+def delete():
+    if ('user' in session and session['user'] == params['admin_user']):
+        post = Posts.query.filter_by(sno=sno).first()
+        db.session.delete(post)
+        db.session.commit()
+    return redirect('/dashboard')
 
 @app.route("/contact", methods = ['GET', 'POST'])
 def contact():
